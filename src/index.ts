7fc78e5034183c57fb604a912ab03331d799b38f
mehdi <mehdi@seald.io>
2022-08-05 14:51:58 +0200

    3

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
-export default (args: {
-  /** description of argString */
-  argString: string,
-  /** description of argStringOpt */
-  argStringOpt?: string,
-  /** description of argNumber */
-  argNumber: number
-  /** description of argFuncWithInlineType */
-  argFuncWithInlineType: () => void
-  /** description of argFuncWithNamedType */
-  argFuncWithNamedType: MyCallBack
-  argWithoutDesc: number
-}) => {}
+export default (
+  /** description of directArgFuncWithInlineType */
+  directArgFuncWithInlineType: () => void,
+  /** description of args */
+  args: {
+    /** description of argString */
+    argString: string,
+    /** description of argStringOpt */
+    argStringOpt?: string,
+    /** description of argNumber */
+    argNumber: number
+    /** description of argFuncWithInlineType */
+    argFuncWithInlineType: () => void
+    /** description of argFuncWithNamedType */
+    argFuncWithNamedType: MyCallBack
+    argWithoutDesc: number
+  }
+) => {}
 
 export type MyCallBack = () => void
 
